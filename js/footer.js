ef9c1ed9edeafdfdb37c7362f36b9a06e52f1321
Nobnag <113401069+Nobnag@users.noreply.github.com>
2022-10-04 18:04:22 +0900

    gnb, footer 수정

--- js/footer.js
+++ js/footer.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
     html += '<div class="footer_container">'
     html += '<div class="footer_top">Science Bookshop GALDAR</div>';
     html += '<div class="footer_content">';
-    html += '<div class="footer_link">상호: 주식회사 갈다 | 대표: 이명현 | 개인정보관리책임자: 김가묵 | 전화: 02-723-1018 | 이메일: galdarbookshop@gmail.com</div>';
-    html += '<div class="footer_link">주소: 서울특별시 종로구 삼청로 10길 18(삼청로) | 사업자등록번호: 309-88-00676 | 통신판매: 제 2017-서울종로-0640 호</div>';
-    html += '<div class="footer_link">호스팅제공자: (주)크리에이터링크 | 이용약관 | 개인정보 처리방침</div>';
+    html += '<div class="footer_link"> 상호: 주식회사 갈다 | 대표: 이명현 | 개인정보관리책임자: 김가묵 | 전화: 02-723-1018 | 이메일: galdarbookshop@gmail.com</div>';
+    html += '<div class="footer_link"> 주소: 서울특별시 종로구 삼청로 10길 18(삼청로) | 사업자등록번호: 309-88-00676 | 통신판매: 제 2017-서울종로-0640 호</div>';
+    html += '<div class="footer_link"> 호스팅제공자: (주)크리에이터링크 | 이용약관 | 개인정보 처리방침</div>';
     html += '</div>'
     html += '</div>'
     html += '</div>'
