b3a424890d5e0ff2386ac4692e8757cd494e9b84
Nobnag <113401069+Nobnag@users.noreply.github.com>
2022-10-04 14:26:37 +0900

    gnb_비로그인 1차

--- js/gnb.js
+++ js/gnb.js
@@ -1,14 +1,21 @@
+//html에 넣으시면됩니다
+//<script src="js/gnb.js"></script>
+
 $(document).ready(function(){
     var html = '<div id="gnb">';
-    html += '<img src="galdar_logo.png>'
-    html += '<a href="login.html" class="gnb_r">로그인</a>';
-    html += '<a href="join.html" >회원가입</a>';
-    html += '<div class="gnb_link">';
-    html += '<a href="https://galdar.kr/">HOME</a>';
-    html += '<a href="login.html">ABOUT</a>';
-    html += '<a href="join.html">LIST</a>';
+    html += '<div class="gnb_container">'
+    html += '<a href="https://galdar.kr/" class="gnb_home_link">';
+    html += '<img src="galdar_logo.png" class="gnb_logo" ></a>';
+    html += '<div class="gnb_content">';
+    html += '<a href="https://galdar.kr/" class="gnb_link">HOME</a>';
+    html += '<a href="login.html" class="gnb_link">ABOUT</a>';
+    html += '<a href="join.html" class="gnb_link">LIST</a>';
     html += '</div>';
-    html += '</div>';
-   
+    html += '<div class="gnb_right">';
+    html += '<a class="gnb_btn_login" href="login.html"><button>로그인</button></a>'
+    html += '<a class="gnb_btn_login" href="join.html"><button>회원가입</button></a>'
+    html += '</div>'
+    html += '</div>'
+    html += '</div>'
     $("body").prepend(html);
 });
